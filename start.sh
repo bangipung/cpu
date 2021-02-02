32e57e839568cf29ba25733274e5c893f10bf4b4
bangipung <78402795+bangipung@users.noreply.github.com>
2021-02-02 14:52:51 +0800

    merubah address wallet

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DGBeQ4sAoq7PuyKYYKNSM2PtMpUwDFm67i -p c=DOGE
+./cpuminer-sse2 -a yespowerSUGAR -o stratum+tcps://stratum-asia.rplant.xyz:17042 -u sugar1qn8kjn2rv43d4v7y4pp4pqcaskzs8y94ucuea6v.fata
 
 
 done
